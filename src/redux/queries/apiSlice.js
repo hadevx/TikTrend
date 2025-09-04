ef9b7dc7c529f3cf978a5744f4d91a8c8b873f27
Hussain <hkosaimi@gmail.com>
2025-09-05 01:02:55 +0300

    fixing baseURL

--- src/redux/queries/apiSlice.js
+++ src/redux/queries/apiSlice.js
@@ -1,12 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl:
-    import.meta.env.VITE_ENVIRONMENT === "development"
-      ? "http://localhost:4001"
-      : "https://backend.webschema.online",
   // baseUrl: "https://backend-production-9357.up.railway.app",
-  // baseUrl: "https://backend.webschema.online",
+  baseUrl: "https://backend.webschema.online",
   credentials: "include",
 });
 
